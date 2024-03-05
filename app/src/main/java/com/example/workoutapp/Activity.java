55213cb2a10353adb3dd732387451d1b40cebf41
tom1dev <tdev429@aucklanduni.ac.nz>
2024-03-05 17:38:07 +1300

    added score and basic functionality to activity object

--- app/src/main/java/com/example/workoutapp/Activity.java
+++ app/src/main/java/com/example/workoutapp/Activity.java
@@ -1,11 +1,21 @@
 package com.example.workoutapp;
 
+import java.util.ArrayList;
+
 class Activity{
     private String name;
     private boolean includesWeight;
+    Double currentsetscore;
+
+    //list of set info (Rep,Weight)
+    ArrayList<ArrayList<Double>> currentSetList;
+
+    ArrayList<ArrayList<Double>> previousSetLists;
     public Activity(String name, boolean includesWeight){
         this.name = name;
         this.includesWeight = includesWeight;
+        currentSetList = new ArrayList<ArrayList<Double>>();
+        //Add this activity to DB or Find DB infomation
     }
 
     public String getName(){
@@ -16,11 +26,23 @@ class Activity{
         return includesWeight;
     }
 
-    public void logWorkout(){
-
+    public void inputSet(int rep,double weight){
+        ArrayList<Double> newSet = new ArrayList<Double>();
+        newSet.add(Double.valueOf(rep));
+        newSet.add(weight);
+        currentSetList.add(newSet);
     }
 
-    private void calculateScore(){
+    private Double calculateScore(ArrayList<ArrayList<Double>> SetList){
+        double SetScore = 0;
 
+        for(int i = 0; i<SetList.size();i++){
+            SetScore += SetList.get(i).get(0) * SetList.get(i).get(1);
+        }
+        return SetScore;
+    }
+
+    private Double ScoreDifference(){
+        return calculateScore(currentSetList)-calculateScore(previousSetLists);
     }
 }
